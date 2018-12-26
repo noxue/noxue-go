0dcfcda4dad2fab38e24ae82bbed9541f6ca2a6a
noxue <yes@noxue.com>
2018-12-26 09:47:02 +0800

    使用vgo控制包版本

--- app.go
+++ app.go
@@ -2,19 +2,72 @@ package main
 
 import (
 	"fmt"
-	"noxue/model"
+	"io/ioutil"
+	"net/http"
+
+	"golang.org/x/oauth2"
 )
 
+const htmlIndex = `<html><body>
+<a href="/GithubLogin">Log in with Github</a>
+</body></html>
+`
+
+// Endpoint is Github's OAuth 2.0 endpoint.
+var endpoint = oauth2.Endpoint{
+	AuthURL:  "https://github.com/login/oauth/authorize",
+	TokenURL: "https://github.com/login/oauth/access_token",
+}
+
+var githubOauthConfig = &oauth2.Config{
+	ClientID:     "f4916bb66a237fcd99ab",
+	ClientSecret: "0d89ba35c753ca0c72b870f04bd6b8f29cb7caac",
+	RedirectURL:  "http://localhost:8000/GithubCallback",
+	Scopes:       []string{"user", "repo"},
+	Endpoint:     endpoint,
+}
+
+const oauthStateString = "random"
+
 func main() {
-	a := &model.Tag{
-		Name:"asdfasdf",
+	http.HandleFunc("/", handleMain)
+	http.HandleFunc("/GithubLogin", handleGithubLogin)
+	http.HandleFunc("/GithubCallback", handleGithubCallback)
+	fmt.Println(http.ListenAndServe(":8000", nil))
+}
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, htmlIndex)
+}
+
+func handleGithubLogin(w http.ResponseWriter, r *http.Request) {
+	url := githubOauthConfig.AuthCodeURL(oauthStateString)
+	fmt.Println(url)
+	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+}
+
+func handleGithubCallback(w http.ResponseWriter, r *http.Request) {
+	state := r.FormValue("state")
+	if state != oauthStateString {
+		fmt.Printf("invalid oauth state, expected '%s', got '%s'\n", oauthStateString, state)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
 	}
-	a.SetDoc(a)
-	err:=a.Save()
+	fmt.Println(state)
 
-	var b model.Tag
+	code := r.FormValue("code")
+	fmt.Println(code)
+	token, err := githubOauthConfig.Exchange(oauth2.NoContext, code)
+	fmt.Println(token)
+	if err != nil {
+		fmt.Printf("Code exchange failed with '%s'\n", err)
+		http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+		return
+	}
 
-	err = a.FindByPk("5c22bd18311f62802e8aa332",&b)
+	response, err := http.Get("https://api.github.com/user?access_token=" + token.AccessToken)
 
-	fmt.Println(b,err,b.Id.Hex())
+	defer response.Body.Close()
+	contents, err := ioutil.ReadAll(response.Body)
+	fmt.Fprintf(w, "%s\n", contents)
 }
---
+++ go.mod
@@ -0,0 +1,13 @@
+module noxue
+
+require (
+	github.com/kr/pretty v0.1.0 // indirect
+	github.com/noxue/mgodb v0.0.0-20150303183006-591245875616
+	golang.org/x/net v0.0.0-20181220203305-927f97764cc3 // indirect
+	golang.org/x/oauth2 v0.0.0-20181203162652-d668ce993890
+	golang.org/x/sync v0.0.0-20181221193216-37e7f081c4d4 // indirect
+	google.golang.org/appengine v1.4.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
+	gopkg.in/yaml.v2 v2.2.2 // indirect
+)
